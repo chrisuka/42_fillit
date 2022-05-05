8ca1a1b846326dab69707a9a486e18f7dd225e14
ikarjala <ikarjala@student.hive.fi>
2022-05-05 18:04:30 +0300

    bulk 2: begin grid solver and printer

--- src/printer.c
+++ src/printer.c
@@ -6,7 +6,7 @@
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 18:26:23 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/04/25 16:47:28 by ikarjala         ###   ########.fr       */
+/*   Updated: 2022/05/05 17:51:50 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,34 @@ int	display_usage(void)
 	const char	*msg = "usage: fillit <tetrimino_file>";
 
 	ft_putendl_fd(msg, FD_OUT);
+	return (XC_EXIT);
+}
+
+int	print_grid(uint16_t *map, uint8_t size, t_tet *tetris)
+{
+	char	*buf;
+	uint8_t	sqr;
+	uint8_t	i;
+	uint8_t	x;
+	uint8_t	y;
+
+	sqr = size * size;
+	buf = (char *)malloc(sizeof(char) * sqr + 1);
+	if (!buf)
+		return (XC_ERROR);
+	i = -1U;
+	y = -1U;
+	while (++y < size)
+	{
+		x = -1U;
+		while (++x < size)
+		{
+			buf[++i] = ".#"[((map[y] & (1 << x)) != 0)];
+			// TODO: needs to print correct tetrimino letter
+		}
+	}
+	buf[sqr] = '\n';
+	write(FD_OUT, buf, sqr);
+	ft_memdel(&buf);
 	return (XC_EXIT);
 }
--- src/solver.c
+++ src/solver.c
@@ -12,57 +12,31 @@
 
 #include "fillit.h"
 
-/* NOTE: we can optimize the fitting further by precalculating
-** the offset based on tet bounds and simply skipping some iterations
-** therefore we will not need to check here if tet is inside grid
-*/
-static int	fit_piece(t_m4x16 chunk, t_tet piece, t_uint size)
+int	solve(uint16_t *map, t_tet *tetris, uint8_t size)
 {
-	if (piece.x + piece.w > size
-		|| piece.y + piece.h > size)
-		return (FT_FALSE);
-	return (piece.bits & chunk == 0);
-}
-
-// BAD IDEA! prefer higher memory usage over more processing per iteration!
-//	TODO: mod tr_bitstr16 to return 64 with the correct line width instead
-/* static uint64_t	tet_expand_rows(uint16_t b_in)
-{
-	uint64_t	b_out;
-	uint8_t		i;
-
-	b_out = (uint64_t)b_in;
-	i = 0;
-	while ((1 << i) & )
-} */
-
-int	solve(t_m4x16 *map, t_tet *tetris, t_uint size)
-{
+	t_m4x16		*m_chunk;
 	uint64_t	bits;
-	t_uint		chunks;
-	t_uint		i;
 	t_uint		x;
 	t_uint		y;
 
-	bits = tet_expand_rows(tetris->bits); // SEE LINE 27
-	if (!bits)
+	if (!tetris->bits)
 		return (FT_TRUE);
-	chunks = size / 4; // can be passed as parameter
-	i = 0;
-	while (!fit_piece(map[i], bits, size))
+	y = -1U;
+	while (++y < size)
 	{
-		x = 0;
-		y = 0;
-		if (x++ < size)
-			bits <<= 1;
-		else
+		m_chunk = (t_m4x16 *)&map[y];
+		x = -1U;
+		while (++x < size)
 		{
-			bits <<= tetris->w;
-			y++;
-			x = 0;
+			bits = (tetris->bits << x);
+			if ((*m_chunk & bits) == 0)
+			{
+				*m_chunk ^= bits;
+				if (solve(map, &tetris[1], size))
+					return (FT_TRUE);
+				*m_chunk ^= bits;
+			}
 		}
-		if (y >= size)
-			return (FT_FALSE);
 	}
-	return (solve(map, &tetris[1], size));
+	return (FT_FALSE);
 }
