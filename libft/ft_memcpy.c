00fea4f9e375e3e9d1be173bc3f44805f886f73f
ikarjala <ikarjala@student.hive.fi>
2022-05-16 16:08:13 +0300

    bulk 1: optimize memset and bzero (doubleword)

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikarjala <ikarjala@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/06 21:31:06 by ikarjala          #+#    #+#             */
-/*   Updated: 2022/05/13 20:55:10 by ikarjala         ###   ########.fr       */
+/*   Created: 2021/11/10 18:49:38 by ikarjala          #+#    #+#             */
+/*   Updated: 2022/03/20 03:10:38 by ikarjala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	uint64_t	*dword_p;
+	size_t	i;
 
-	dword_p = &s[n];
-	while (n >= sizeof(dword_p))
-	{
-		n -= sizeof(dword_p);
-		*(--dword_p) = 0x0ULL;
-	}
-	while (n-- > 0)
-		((unsigned char *)s)[n] = 0;
+	if (dst == NULL && src == NULL)
+		return (NULL);
+	if (n == 0 || dst == src)
+		return (dst);
+	i = -1UL;
+	while (++i < n)
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+	return (dst);
 }
